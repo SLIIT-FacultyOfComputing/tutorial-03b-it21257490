35ab640b98779fc07eb907b55eb4228fd32ad295
Sanindu Rukshan Rathnayke <99402806+it21257490@users.noreply.github.com>
2022-03-18 16:42:48 +0000

    updated

--- exercise03.cpp
+++ exercise03.cpp
@@ -2,12 +2,14 @@
 
 using namespace std;
 
+struct Box{
+  int height;
+  int width;
+  int length;
+}box1,box2;
+
 int volume(int height, int width, int length);
 
-// 1. Define a structure called Box
-// have the integer data types Height, Width, Length
-
-// Do not change the main function
 int main() {
     // 2. Create a variable called box1 of the Box structure type
     // int box1Height, box1Width, box1Length;
@@ -17,23 +19,23 @@ int main() {
     int totalVolume;
     
     // 4. Input the height, width, lenght of box1 and box2
-    cout << "Enter Box 1 Height : ";
-    cin >> Height of Box 1;
-    cout << "Enter Box 1 Width : ";
-    cin >> Width of Box 1;
-    cout << "Enter Box 1 Length : ";
-    cin >> Length of Box 1;
+    cout << "Enter Box 1 Height : "<<endl;
+    cin >> box1.height;
+    cout << "Enter Box 1 Width : "<<endl;
+    cin >> box1.width;
+    cout << "Enter Box 1 Length : "<<endl;
+    cin >> box1.length;
     
-    cout << "Enter Box 2 Height : ";
-    cin >> Height of Box 2;
-    cout << "Enter Box 2 Width : ";
-    cin >> Width of Box 2;
-    cout << "Enter Box 2 Length : ";
-    cin >> Length of Box 2;
+    cout << "Enter Box 2 Height : "<<endl;
+    cin >> box2.height;
+    cout << "Enter Box 2 Width : "<<endl;
+    cin >> box2.width;
+    cout << "Enter Box 2 Length : "<<endl;
+    cin >> box2.length;
     
     // 5. Replace the coding below to pass the Box type structure
-    totalVolume = volume( , , )
-             + volume( , , );
+    totalVolume = volume(box1.height ,box1.width ,box1.length )
+             + volume( box2.height,box2.width, box2.length );
     
     cout << "Volume of Box is " << totalVolume << endl;
     
@@ -41,3 +43,9 @@ int main() {
 }
 
 // Implement the functions here
+
+int volume(int height, int width, int length)
+{
+  return(height*width*length);
+}
+
